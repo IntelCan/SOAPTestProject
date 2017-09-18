cbcd248fe7d1edcf44f3f244daf2677716ff98ce
Arek Boruch <arkadiusz.boruch@britenet.com.pl>
2017-09-18 12:43:45 +0200

    Patch

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -2,13 +2,17 @@ package example;
 
 
 import model.UserAccount;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import service.UserAccountServiceImpl;
 
 import javax.xml.ws.Endpoint;
 
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
         UserAccountServiceImpl userAccountService = new UserAccountServiceImpl();
         String addressUserAccount = "http://localhost:9000/userAccountService";
         Endpoint.publish(addressUserAccount, userAccountService);
