951a1e0e619b723be9483e523f06106a26b4f024
Arek Boruch <arkadiusz.boruch@britenet.com.pl>
2017-09-19 15:11:56 +0200

    A few changes

--- src/main/java/soap/model/UserAccount.java
+++ src/main/java/soap/model/UserAccount.java
@@ -2,13 +2,11 @@ package soap.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
+@SequenceGenerator(name="users_seq_name",sequenceName="users_seq", allocationSize=1, initialValue = 5)
 public class UserAccount {
 
     @Id
